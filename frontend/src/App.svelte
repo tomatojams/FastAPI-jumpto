5bbae22511ce85c84c582ab68c8a7e90a149a596
tomatojams <tomatojams@naver.com>
2024-02-28 23:39:40 +0900

    first commit

---
+++ frontend/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  let message;
+
+  fetch("http://127.0.0.1:8000/hello").then((response) => {
+    response.json().then((json) => {
+      message = json.message;
+    });
+  });
+</script>
+
+<h1>{message}</h1>
+
+<!-- 비동기식 -->
+<!-- <script>
+  async function hello() {
+    const res = await fetch("http://127.0.0.1:8000/hello");
+    const json = await res.json();
+
+    if (res.ok) {
+      return json.message;
+    } else {
+      alert("error");
+    }
+  }
+
+  let promise = hello();
+</script>
+
+{#await promise}
+  <p>...waiting</p>
+{:then message}
+  <h1>{message}</h1>
+{/await} -->
---
+++ main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    #"http://127.0.0.1:5173",    # 또는
+    "http://localhost:5173"
+]
+
+app.add_middleware(  # CORS 설정 예외주소
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/hello")
+def hello():
+    return {"message": "안녕하세요 파이보"}
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+# @app.get("/hello/{name}")
+# async def say_hello(name: str):
+#     return {"message": f"Hello {name}"}
