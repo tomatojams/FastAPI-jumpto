01724883868c3fb57bfc1a1643f28055bfd5d902
tomatojams <tomatojams@naver.com>
2024-02-29 10:53:50 +0900

    FastAPI alembic설치

---
+++ temp.py
@@ -0,0 +1,10 @@
+# Question 테이블에 id가 2인 데이터를 가져와서, Answer 테이블에 추가하는 코드
+
+# from datetime import datetime
+# from models import Question, Answer
+# from database import SessionLocal
+# db = SessionLocal()
+# q = db.query(Question).get(2)
+# a = Answer(question=q, content='네 자동으로 생성됩니다.', create_date=datetime.now())
+# db.add(a)
+# db.commit()
