a34928e0cdd3bece946607bb7583d39d1dcead94
tomatojams <tomatojams@naver.com>
2024-03-01 23:20:32 +0900

    FastAPI

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -1,7 +1,6 @@
 from models import Question
 from sqlalchemy.orm import Session
 
-
 def get_question_list(db: Session):
     question_list = db.query(Question) \
 .order_by(Question.create_date.desc()) \
